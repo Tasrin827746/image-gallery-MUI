e6cc709dcad548d076448901a75171030d1e1a44
Morsala Tasrin <morsalatasrin@gmail.com>
2025-04-11 11:51:50 +0600

    ....

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,3 @@
-import arcjet, { detectBot, request } from "@/libs/Arcjet";
-import { Env } from "@/libs/Env";
 import "@/styles/global.css";
 import type { Metadata } from "next";
 
@@ -28,40 +26,11 @@ export const metadata: Metadata = {
   ],
 };
 
-// Improve security with Arcjet
-const aj = arcjet.withRule(
-  detectBot({
-    mode: "LIVE",
-    // Block all bots except the following
-    allow: [
-      // See https://docs.arcjet.com/bot-protection/identifying-bots
-      "CATEGORY:SEARCH_ENGINE", // Allow search engines
-      "CATEGORY:PREVIEW", // Allow preview links to show OG images
-      "CATEGORY:MONITOR", // Allow uptime monitoring services
-    ],
-  })
-);
-
 export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  // Verify the request with Arcjet
-  if (Env.ARCJET_KEY) {
-    const req = await request();
-    const decision = await aj.protect(req);
-
-    // These errors are handled by the global error boundary, but you could also
-    // redirect or show a custom error page
-    if (decision.isDenied()) {
-      if (decision.reason.isBot()) {
-        throw new Error("No bots allowed");
-      }
-
-      throw new Error("Access denied");
-    }
-  }
 
   return (
     <html lang="en">
